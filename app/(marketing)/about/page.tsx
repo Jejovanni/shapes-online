992a3edec3f6398b3015b13fe6116c0d738cc34a
jejovanni <jamaineekong@gmail.com>
2026-02-06 16:27:48 +0100

    react node import issue fix

--- app/(marketing)/about/page.tsx
+++ app/(marketing)/about/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from 'react';
+import React, { ReactNode } from 'react'; // This line is the fix
 import Image from 'next/image';
 import { ShieldCheck, Target, MessageSquareText } from 'lucide-react';
 
